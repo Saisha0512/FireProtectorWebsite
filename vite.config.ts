72f65c5da06107331fd8b4bf4201960cbad8271a
Saisha Verma <saishaverma0512@gmail.com>
2025-11-30 02:51:52 +0530

    Update comment for repository name in config

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 
+// IMPORTANT: put your repository name here
 export default defineConfig({
   plugins: [react()],
-  base: "/FireProtectorWebsite/",   // <-- Your repo name
+  base: "/FireProtectorWebsite/",   // <-- your repo name exactly
 });
